3d4d897e7f72e498ce43314f6047ad1389a5706e
Nini Liao <u9513031@cmu.edu.tw>
2020-10-24 05:25:46 +0800

    add app : accounts

--- ninimanager/ninimanager/settings.py
+++ ninimanager/ninimanager/settings.py
@@ -39,8 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'leads',
     'rest_framework',
-    'frontend'
+    'frontend',
+    'knox',
+    'accounts'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,5 +127,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
- 
